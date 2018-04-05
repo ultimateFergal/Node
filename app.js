798040be9eb62c1090e130ea4837af9749c2c0df
Luis Fernando Galarcio Marin <fernando.galarcio@gmail.com>
2018-04-04 22:22:53 -0500

    api o site?

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const express = require('express');
 const app = express();
 
 //En esa dirección la función recibirá peticiones y respuestas
-app.get('/', (req, res) => {
+//para una web api
+/* app.get('/', (req, res) => {
     res.end('works!');
-});
+}); */
+
+//Para usarlo como sitio web
+//Usa los archivos estáticos en el carpeta publics
+app.use(express.static('public'));
 
 //Abrir en localhost:3000
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Servidor iniciado.')
+});
