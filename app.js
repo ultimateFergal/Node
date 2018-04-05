2ceefdeeb6b795ff8eeb2b8dad223cb5dceed85c
Luis Fernando Galarcio Marin <fernando.galarcio@gmail.com>
2018-04-04 23:49:55 -0500

    Nota

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app     = express();
 
-
+//API
 /* app.get('/', (req, res) => {
     res.end('works!');
 }); */
+
+//SITE
 app.use(express.static('public'))//le digo que use los archivos estaticos de mi carpeta public
 
 app.listen(3000, () => {
